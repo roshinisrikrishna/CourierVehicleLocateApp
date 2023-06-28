6d8e8a1650b6029acac76827179acb81ffaf44a8
roshinisrikrishna <38094811+roshinisrikrishna@users.noreply.github.com>
2023-06-28 14:45:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,14 +21,11 @@ const db = mysql.createPool({
 
 
 app.use(cors({
-<<<<<<< HEAD
-  origin: "http://www.couriertrip.trackman.in",
-=======
-  origin: "http://couriertrip.trackman.in",
->>>>>>> 5532e6af2232fda7707883e4d4a59db6829768a1
-  methods: ["POST", "GET","PUT","DELETE"],
+  origin: "http://www.couriertrip.trackman.in", // Updated origin value
+  methods: ["POST", "GET", "PUT", "DELETE"],
   credentials: true
 }));
+
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
